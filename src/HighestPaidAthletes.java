e216d8808fb060c3a3ef98503673301b6444bcee
jacobprunes <jprunes03@gmail.com>
2021-01-06 10:16:34 -0500

    Complete Part 1 with G & S + system out

--- src/HighestPaidAthletes.java
+++ src/HighestPaidAthletes.java
@@ -5,8 +5,8 @@ public class HighestPaidAthletes {
         AllTimeAthlete athlete1 = new AllTimeAthlete(1, "Michael Jordan", "Basketball", "United States", 1850);
         TwentyTwentyAthlete athlete2 = new TwentyTwentyAthlete(1, "Roger Federer", "Tennis", "Switzerland", 106, 6, 100);
 
-        System.out.println("\"" + athlete1.getName() + "\" is ranked #" + athlete1.getRank() + ". He plays " + athlete1.getSport() + " in " + athlete1.getNation() + " and has earned " + athlete1.getEarnings() + " in Millions.");
-
+        System.out.println(athlete1);
+        System.out.println(athlete2);
 
     }
 }
